a629219cae28a6100574692ed5497ecd55f0f0ec
Alves <jvmmalves@gmail.com>
2025-08-31 21:29:06 -0300

    adicionei composição

--- Item.hpp
+++ Item.hpp
@@ -1,15 +1,18 @@
 #pragma once
 #include <iostream>
+#include "Cliente.hpp"
 
 class Item
 {
     std::string editoraItem, precoItem, categoriaItem;
+    Cliente* dono;
 
 public:
     Item(std::string _editoraItem, std::string _precoItem, std::string _categoriaItem){
         this->editoraItem = _editoraItem;
         this->precoItem = _precoItem;
         this->categoriaItem = _categoriaItem;
+        this->dono = nullptr;
     }
     
     //pega e coloca editora
@@ -36,10 +39,26 @@ public:
         categoriaItem = _categoriaItem;
     }
 
+    void setDono(Cliente* _dono) {
+        dono = _dono; 
+    }
+    Cliente* getDono() const { 
+        return dono; 
+    }
+    bool temDono() const { 
+        return dono != nullptr; 
+    }
+
     virtual void exibir() {
         std::cout << "Editora: " << editoraItem <<
         "Preço: " << precoItem <<
         "Categoria: " << categoriaItem;
+        
+        if (temDono()) {
+            std::cout << "Dono: [ID " << dono->getId() << "] " << dono->getNome() << "\n";
+        } else {
+            std::cout << "Item disponível (sem dono)\n";
+        }
     }
 
     virtual ~Item() = default;
